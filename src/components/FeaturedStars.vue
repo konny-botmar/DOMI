5592eb98f949e46d1711968f7771eb443d321e92
yenny constanza botero <konnybotmar2@gmail.com>
2022-10-13 20:19:43 -0500

    styling changes to featuredStars component

--- src/components/FeaturedStars.vue
+++ src/components/FeaturedStars.vue
@@ -3,76 +3,84 @@
             <h2 class="featured-title">DESTACADOS</h2>
             <div class="featured-grid">
                 <div class="featured-item">
-                    <img src="https://i.postimg.cc/x81YzQxq/de-la-monta-a.jpg" alt="DE LA MONTAÑA">
-                    <h3>DE LA MONTAÑA</h3>
-                    <form>
-                        <p class="clasificacion">
-                        <input id="radio1" type="radio" name="estrellas" value="5">
-                            <label for="radio1">★</label>
-                            <input id="radio2" type="radio" name="estrellas" value="4">
-                            <label for="radio2">★</label>
-                            <input id="radio3" type="radio" name="estrellas" value="3">
-                            <label for="radio3">★</label>
-                            <input id="radio4" type="radio" name="estrellas" value="2">
-                            <label for="radio4">★</label>
-                            <input id="radio5" type="radio" name="estrellas" value="1">
-                            <label for="radio5">★</label>
-                        </p>
-                    </form>
+                    <a href="">
+                        <img src="https://i.postimg.cc/x81YzQxq/de-la-monta-a.jpg" alt="DE LA MONTAÑA">
+                        <h3>DE LA MONTAÑA</h3>
+                    </a>
+                        <form>
+                            <p class="clasificacion">
+                            <input id="radio1" type="radio" name="estrellas" value="5">
+                                <label for="radio1">★</label>
+                                <input id="radio2" type="radio" name="estrellas" value="4">
+                                <label for="radio2">★</label>
+                                <input id="radio3" type="radio" name="estrellas" value="3">
+                                <label for="radio3">★</label>
+                                <input id="radio4" type="radio" name="estrellas" value="2">
+                                <label for="radio4">★</label>
+                                <input id="radio5" type="radio" name="estrellas" value="1">
+                                <label for="radio5">★</label>
+                            </p>
+                        </form>
                 </div>
                 <div class="featured-item">
-                    <img src="https://i.postimg.cc/GtgRPfVK/la-gula.jpg" alt="LA GULA">
-                    <h3>LA GULA</h3>
-                    <form>
-                        <p class="clasificacion">
-                        <input id="radio1" type="radio" name="estrellas" value="5">
-                            <label for="radio1">★</label>
-                            <input id="radio2" type="radio" name="estrellas" value="4">
-                            <label for="radio2">★</label>
-                            <input id="radio3" type="radio" name="estrellas" value="3">
-                            <label for="radio3">★</label>
-                            <input id="radio4" type="radio" name="estrellas" value="2">
-                            <label for="radio4">★</label>
-                            <input id="radio5" type="radio" name="estrellas" value="1">
-                            <label for="radio5">★</label>
-                        </p>
-                    </form>
+                    <a href="">
+                        <img src="https://i.postimg.cc/GtgRPfVK/la-gula.jpg" alt="LA GULA">
+                        <h3>LA GULA</h3>
+                    </a>
+                        <form>
+                            <p class="clasificacion">
+                            <input id="radio1" type="radio" name="estrellas" value="5">
+                                <label for="radio1">★</label>
+                                <input id="radio2" type="radio" name="estrellas" value="4">
+                                <label for="radio2">★</label>
+                                <input id="radio3" type="radio" name="estrellas" value="3">
+                                <label for="radio3">★</label>
+                                <input id="radio4" type="radio" name="estrellas" value="2">
+                                <label for="radio4">★</label>
+                                <input id="radio5" type="radio" name="estrellas" value="1">
+                                <label for="radio5">★</label>
+                            </p>
+                        </form>
                 </div>
                 <div class="featured-item">
-                    <img src="https://i.postimg.cc/gJDmjjKf/paz-y-flora.png" alt="PAZ Y FLORA">
-                    <h3>PAZ Y FLORA</h3>
-                    <form>
-                        <p class="clasificacion">
-                        <input id="radio1" type="radio" name="estrellas" value="5">
-                            <label for="radio1">★</label>
-                            <input id="radio2" type="radio" name="estrellas" value="4">
-                            <label for="radio2">★</label>
-                            <input id="radio3" type="radio" name="estrellas" value="3">
-                            <label for="radio3">★</label>
-                            <input id="radio4" type="radio" name="estrellas" value="2">
-                            <label for="radio4">★</label>
-                            <input id="radio5" type="radio" name="estrellas" value="1">
-                            <label for="radio5">★</label>
-                        </p>
-                    </form>
+                    <a href="">
+                        <img src="https://i.postimg.cc/gJDmjjKf/paz-y-flora.png" alt="PAZ Y FLORA">
+                        <h3>PAZ Y FLORA</h3>
+                    </a>
+                        <form>
+                            <p class="clasificacion">
+                            <input id="radio1" type="radio" name="estrellas" value="5">
+                                <label for="radio1">★</label>
+                                <input id="radio2" type="radio" name="estrellas" value="4">
+                                <label for="radio2">★</label>
+                                <input id="radio3" type="radio" name="estrellas" value="3">
+                                <label for="radio3">★</label>
+                                <input id="radio4" type="radio" name="estrellas" value="2">
+                                <label for="radio4">★</label>
+                                <input id="radio5" type="radio" name="estrellas" value="1">
+                                <label for="radio5">★</label>
+                            </p>
+                        </form>
                 </div>
                 <div class="featured-item">
-                    <img src="https://i.postimg.cc/dVssM8mG/TAHONA.jpg" alt="TAHONA">
-                    <h3>TAHONA</h3>
-                    <form>
-                        <p class="clasification">
-                        <input id="radio1" type="radio" name="estrellas" value="5">
-                            <label for="radio1">★</label>
-                            <input id="radio2" type="radio" name="estrellas" value="4">
-                            <label for="radio2">★</label>
-                            <input id="radio3" type="radio" name="estrellas" value="3">
-                            <label for="radio3">★</label>
-                            <input id="radio4" type="radio" name="estrellas" value="2">
-                            <label for="radio4">★</label>
-                            <input id="radio5" type="radio" name="estrellas" value="1">
-                            <label for="radio5">★</label>
-                        </p>
-                    </form>
+                    <a href="">
+                        <img src="https://i.postimg.cc/dVssM8mG/TAHONA.jpg" alt="TAHONA">
+                        <h3>TAHONA</h3>
+                    </a>
+                        <form>
+                            <p class="clasification">
+                            <input id="radio1" type="radio" name="estrellas" value="5">
+                                <label for="radio1">★</label>
+                                <input id="radio2" type="radio" name="estrellas" value="4">
+                                <label for="radio2">★</label>
+                                <input id="radio3" type="radio" name="estrellas" value="3">
+                                <label for="radio3">★</label>
+                                <input id="radio4" type="radio" name="estrellas" value="2">
+                                <label for="radio4">★</label>
+                                <input id="radio5" type="radio" name="estrellas" value="1">
+                                <label for="radio5">★</label>
+                            </p>
+                        </form>
                 </div>
             </div>
         </section>
@@ -82,11 +90,12 @@ export default {
     name: 'FeatureStars',
 }
 </script>
-<style>
+<style scoped>
     section.featured{
     padding: 15rem;
     padding-top: 5rem ;
-    padding-bottom: 0;
+    padding-bottom: 5rem;
+    background-color: #F6DE80;
 }
 
 .featured-title {
@@ -110,6 +119,12 @@ export default {
 .featured-item img {
     width: 15rem;
     border-radius: 100px;
+    box-shadow: 8px 5px 5px rgb(26, 26, 26);
+}
+
+.featured-item h3{
+    color: black;
+    font-weight: bolder;
 }
 
 #form {
